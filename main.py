49bfd3c783100fefae1c6c58d9394a078a067c2b
giobra <gioobrandino@gmail.com>
2026-05-08 20:40:16 -0300

    insercao dos datasets que serao utilizados

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# classificacao -> random forest -> id3
+# scikit learn -> biblioteca com Random forest e MLP
+# usar exemplo do iris dataset para usar o seu dataset
+# talvez dividir em um faz o código e o outro os teste (RMSE, precision, recall, f-measure, acurácia, matriz de confusão)
+
 def main():
     print("Hello from resgate!")
 
